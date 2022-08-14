739c4e8bda1c9783b665ef10d241196d3e73232c
OceanLeonAI <beyondjayfir@126.com>
2022-08-14 21:07:35 +0800

    initial the repository hello-mybatis-generator

---
+++ src/main/java/com/leon/HelloMybatisGenerator.java
@@ -0,0 +1,17 @@
+package com.leon;
+
+/**
+ * @PROJECT_NAME: hello-mybatis-generator
+ * @CLASS_NAME: HelloMybatisGenerator
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2022/8/14 0014 20:45
+ * @Version 1.0
+ * @DESCRIPTION: hello-mybatis-generator
+ **/
+public class HelloMybatisGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/leon/HelloMybatisGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.leon;
+
+import org.junit.Test;
+
+/**
+ * @PROJECT_NAME: hello-mybatis-generator
+ * @CLASS_NAME: HelloMybatisGeneratorTest
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2022/8/14 0014 20:46
+ * @Version 1.0
+ * @DESCRIPTION: junit
+ **/
+public class HelloMybatisGeneratorTest {
+
+    @Test
+    public void helloWorld() {
+        System.out.println("hello world");
+    }
+
+}
